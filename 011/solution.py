a31f68bf6a444f6518b2ddc0ca2b859f75b6cb85
Xavier Smet <xaviersmet@gmail.com>
2015-07-27 14:13:11 +0200

    Problem 11 solved in Python - answer 70600674

--- 011/solution.py
+++ 011/solution.py
@@ -1,7 +1,31 @@
+n = 4
+
 with open('matrix.txt') as file:
 	lines = [line.strip('\n') for line in file]
 
 for i in xrange(0, len(lines)):
-	lines[i] = map(int, list(lines[i]))
+	lines[i] = map(int, lines[i].split(' '))
 
-print lines
+w = len(lines[1])
+h = len(lines)
+
+maxprod=0
+for i in range(w-n+1): #i = 0..16
+	for j in range(h-n+1): #j = 0..16
+		prodhorz=1
+		proddia=1
+		prodvert=1
+		proddia2=1
+		for k in range(n): #k = 0..3
+			prodhorz *= lines[i][j+k]
+			proddia  *= lines[i+k][j+k]
+			prodvert *= lines[i+k][j]
+			proddia2 *= lines[i+k][j+n-1-k]
+
+		thismax = max([prodhorz, prodvert, proddia, proddia2])
+
+		if thismax > maxprod:
+			print "%d, %d (%d): %d" % (i, j, lines[i][j], thismax)
+			maxprod=thismax
+
+print maxprod
